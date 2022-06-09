9e68a82a84c3c492158c09f6a000dbb0510643ab
sandro <devspuig@pm.me>
2022-06-09 17:18:57 +0200

    Remove outdated config

--- api/www/src/config/config.service.ts
+++ api/www/src/config/config.service.ts
@@ -5,22 +5,6 @@ import { ConfigService as NestJSConfigService } from '@nestjs/config';
 export class ConfigService {
   constructor(private configService: NestJSConfigService) {}
 
-  public sqsQueueUrl() {
-    return this.value('sqs_queue_url');
-  }
-
-  public region() {
-    return this.value('region');
-  }
-
-  public accessKey() {
-    return this.value('access_key');
-  }
-
-  public secret() {
-    return this.value('secret');
-  }
-
   private isProduction(): boolean {
     return this.value('environment') === 'production';
   }
--- api/www/src/config/configuration.ts
+++ api/www/src/config/configuration.ts
@@ -2,8 +2,4 @@ import 'dotenv/config';
 
 export default () => ({
   env: process.env.NODE_ENV || 'development',
-  sqs_queue_url: process.env.SQS_QUEUE_URL,
-  region: process.env.REGION,
-  access_key: process.env.ACCESS_KEY,
-  secret: process.env.SECRET,
 });
