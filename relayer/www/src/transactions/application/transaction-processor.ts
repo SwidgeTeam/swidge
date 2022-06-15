71757f49ef6abf58f8355eea3ddc3bf53f51e1a0
sandro <devspuig@pm.me>
2022-06-15 23:16:49 +0200

    Improve code readability

--- relayer/www/src/transactions/application/transaction-processor.ts
+++ relayer/www/src/transactions/application/transaction-processor.ts
@@ -21,37 +21,7 @@ export class TransactionProcessor {
   async execute(job: TransactionJob) {
     this.logger.log('Received job ', job);
 
-    let swapDetails;
-
-    if (job.srcToken === job.dstToken) {
-      swapDetails = {
-        providerCode: null,
-        amountIn: job.bridgeAmountOut,
-        tokenIn: job.srcToken,
-        tokenOut: job.dstToken,
-        estimatedGas: '',
-        data: '0x',
-        required: false,
-      };
-    } else {
-      const swapOrder = await this.transactionRepository.quoteSwap(<
-        SwapRequest
-      >{
-        chainId: job.toChainId,
-        tokenIn: job.srcToken,
-        tokenOut: job.dstToken,
-        amountIn: job.bridgeAmountOut,
-      });
-      swapDetails = {
-        providerCode: swapOrder.providerCode,
-        amountIn: job.bridgeAmountOut,
-        tokenIn: swapOrder.tokenIn,
-        tokenOut: swapOrder.tokenOut,
-        data: swapOrder.data,
-        estimatedGas: swapOrder.estimatedGas,
-        required: swapOrder.required,
-      };
-    }
+    const swapDetails = await this.getSwapDetails(job);
 
     // Execute transaction
     const params: FinalizeCrossParams = {
@@ -67,5 +37,43 @@ export class TransactionProcessor {
     await this.routerCaller.call(params);
 
     this.logger.log('Tx executed');
+  }
+
+  /**
+   * Computes the swap details given the received job
+   * @param job
+   * @private
+   */
+  private async getSwapDetails(job: TransactionJob) {
+    if (job.srcToken === job.dstToken) {
+      // If final token is same than the received,
+      // no swap required
+      return {
+        providerCode: null,
+        amountIn: job.bridgeAmountOut,
+        tokenIn: job.srcToken,
+        tokenOut: job.dstToken,
+        estimatedGas: '',
+        data: '0x',
+        required: false,
+      };
+    }
+
+    const swapOrder = await this.transactionRepository.quoteSwap(<SwapRequest>{
+      chainId: job.toChainId,
+      tokenIn: job.srcToken,
+      tokenOut: job.dstToken,
+      amountIn: job.bridgeAmountOut,
+    });
+
+    return {
+      providerCode: swapOrder.providerCode,
+      amountIn: job.bridgeAmountOut,
+      tokenIn: swapOrder.tokenIn,
+      tokenOut: swapOrder.tokenOut,
+      estimatedGas: swapOrder.estimatedGas,
+      data: swapOrder.data,
+      required: swapOrder.required,
+    };
   }
 }
