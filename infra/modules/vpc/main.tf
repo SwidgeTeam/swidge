2836912270187c8777b0236913b451ed81f3fd19
sandro <devspuig@pm.me>
2022-06-25 00:19:26 +0200

    Remove default security group

--- infra/modules/vpc/main.tf
+++ infra/modules/vpc/main.tf
@@ -9,33 +9,6 @@ resource "aws_vpc" "vpc" {
   }
 }
 
-/*== Default Security Group ==*/
-
-resource "aws_security_group" "default" {
-  name        = "${var.environment}-default-sg"
-  description = "Default security group to allow inbound/outbound from the VPC"
-  vpc_id      = aws_vpc.vpc.id
-  depends_on  = [aws_vpc.vpc]
-
-  ingress {
-    from_port = "0"
-    to_port   = "0"
-    protocol  = "-1"
-    self      = true
-  }
-
-  egress {
-    from_port = "0"
-    to_port   = "0"
-    protocol  = "-1"
-    self      = "true"
-  }
-
-  tags = {
-    Environment = var.environment
-  }
-}
-
 output "vpc_id" {
   value = aws_vpc.vpc.id
 }
