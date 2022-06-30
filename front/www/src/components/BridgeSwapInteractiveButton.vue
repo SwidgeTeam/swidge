3550f3091fdd44aff5219008c84927ced9e3e026
Gabriel Pedroso <gabriel.pedroso@masterlink.pt>
2022-06-30 19:36:57 +0100

    Y5won6AQ - showing insufficient balance after quote and fixing interactive button

--- front/www/src/components/BridgeSwapInteractiveButton.vue
+++ front/www/src/components/BridgeSwapInteractiveButton.vue
@@ -1,23 +1,21 @@
-<script setup lang='ts'>
+<script setup lang="ts">
 import LoadingCircle from "./svg/LoadingCircle.vue";
 
 defineProps<{
-  text: string
-  disabled: boolean
-  isLoading: boolean
-  onClick: () => void
-}>()
+  text: string;
+  disabled: boolean;
+  isLoading: boolean;
+  onClick: () => void;
+}>();
 </script>
 
 <template>
   <button
     class="w-full py-3 rounded-xl gradient-main flex items-center justify-center text-3xl"
-    :class="['opacity-50 pointer-events-none']"
-    @click="onClick()">
+    :class="[(disabled || isLoading) && 'opacity-50 pointer-events-none']"
+    @click="onClick()"
+  >
     <span>{{ text }}</span>
-    <LoadingCircle
-      v-if="isLoading"
-      class="h-9 w-9"
-    />
+    <LoadingCircle v-if="isLoading" class="h-9 w-9" />
   </button>
 </template>
