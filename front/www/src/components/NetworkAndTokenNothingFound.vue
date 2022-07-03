8cfc30e14e26d852091a7ba55f78d8cd98535d10
sandro <devspuig@pm.me>
2022-07-03 22:45:32 +0200

    Fix typo

--- front/www/src/components/NetworkAndTokenNothingFound.vue
+++ front/www/src/components/NetworkAndTokenNothingFound.vue
@@ -1,12 +1,12 @@
 <script setup lang='ts'>
 </script>
- 
+
 <template>
   <div class="w-full h-full flex flex-col items-center justify-center gap-4">
     <span class="text-light-grey-1 text-2xl">No tokens to show</span>
     <div class="text-center w-80 text-[#A5A5A5] font-extralight">To see the
       token list
-      please seach for a
+      please search for a
       token
       or select a network
     </div>
