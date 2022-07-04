d93177140525f2d33d9234d6a556de3d37b0011e
sandro <devspuig@pm.me>
2022-07-04 18:27:14 +0200

    Format router-caller

--- front/www/src/contracts/routerCaller.ts
+++ front/www/src/contracts/routerCaller.ts
@@ -1,6 +1,6 @@
-import { ethers, ContractTransaction } from 'ethers';
-import routerAbi from './router.json';
-import IERC20Abi from './IERC20.json';
+import { ethers, ContractTransaction } from 'ethers'
+import routerAbi from './router.json'
+import IERC20Abi from './IERC20.json'
 
 export interface RouterCallPayload {
     router: string
@@ -29,36 +29,36 @@ export const NATIVE_COIN_ADDRESS = '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee'
 
 export class RouterCaller {
 
-    private static MAX_UINT256 = (2 ** 255).toLocaleString('fullwide', { useGrouping: false });
+    private static MAX_UINT256 = (2 ** 255).toLocaleString('fullwide', { useGrouping: false })
 
     static provider() {
         // Get user account
-        return new ethers.providers.Web3Provider(window.ethereum);
+        return new ethers.providers.Web3Provider(window.ethereum)
     }
 
     static async call(params: RouterCallPayload): Promise<ContractTransaction> {
-        const provider = this.provider();
-        const nativeInput = params.originSwap.tokenIn === NATIVE_COIN_ADDRESS;
+        const provider = this.provider()
+        const nativeInput = params.originSwap.tokenIn === NATIVE_COIN_ADDRESS
 
         // Get Router contract instance
         const Router = new ethers.Contract(
             params.router,
             routerAbi,
             provider.getSigner()
-        );
+        )
 
         if (!nativeInput) {
             const tokenIn = params.originSwap.required
                 ? params.originSwap.tokenIn
-                : params.bridge.tokenIn;
-            await RouterCaller.approveIfRequired(tokenIn, params.router);
+                : params.bridge.tokenIn
+            await RouterCaller.approveIfRequired(tokenIn, params.router)
         }
 
-        const feeData = await provider.getFeeData();
+        const feeData = await provider.getFeeData()
 
         const valueToSend = nativeInput
             ? params.amountIn
-            : 0;
+            : 0
 
         // Create transaction
         return Router.initSwidge(
@@ -85,7 +85,7 @@ export class RouterCaller {
                 gasLimit: 2000000, // TODO : set more accurate
                 value: valueToSend
             }
-        );
+        )
     }
 
     /**
@@ -94,26 +94,26 @@ export class RouterCaller {
      * @param spender
      */
     static async approveIfRequired(tokenAddress: string, spender: string) {
-        const provider = this.provider();
-        const signer = provider.getSigner();
+        const provider = this.provider()
+        const signer = provider.getSigner()
 
         // Get token contract
         const Token = new ethers.Contract(
             tokenAddress,
             IERC20Abi,
             signer
-        );
+        )
 
-        const allowance = await Token.allowance(window.ethereum.selectedAddress, spender);
+        const allowance = await Token.allowance(window.ethereum.selectedAddress, spender)
 
         if (allowance.toString() === this.MAX_UINT256) {
-            return;
+            return
         }
 
         // Create the transaction
-        const tx = await Token.approve(spender, this.MAX_UINT256);
+        const tx = await Token.approve(spender, this.MAX_UINT256)
 
         // Broadcast & wait
-        await tx.wait();
+        await tx.wait()
     }
 }
