907a7c3590c3ec53413f24fa83b4084282f8322b
sandro <devspuig@pm.me>
2022-07-05 00:09:47 +0200

    Fix test

--- api/www/test/unit/paths/application/query/get-path.handler.spec.ts
+++ api/www/test/unit/paths/application/query/get-path.handler.spec.ts
@@ -7,6 +7,8 @@ import { Token } from '../../../../../src/shared/domain/Token';
 import { RouterAddressFetcher } from '../../../../../src/addresses/application/query/RouterAddressFetcher';
 import { InsufficientLiquidity } from '../../../../../src/swaps/domain/InsufficientLiquidity';
 import { TokenDetailsFetcher } from '../../../../../src/shared/infrastructure/TokenDetailsFetcher';
+import { PriceFeedConverter } from '../../../../../src/shared/infrastructure/PriceFeedConverter';
+import { BigNumber } from 'ethers';
 
 describe('get path', () => {
   it('should return error if invalid first swap', async () => {
@@ -25,12 +27,18 @@ describe('get path', () => {
         return Promise.resolve(new Token('', '', 6));
       },
     });
+    const mockPriceFeedConverter = createMock<PriceFeedConverter>({
+      fetch: () => {
+        return Promise.resolve(BigNumber.from(0));
+      },
+    });
 
     const handler = new GetPathHandler(
       mockRouterFetcher,
       mockSwapProvider,
       mockBridgeProvider,
       mockTokenDetailsFetcher,
+      mockPriceFeedConverter,
     );
 
     const query = new GetPathQuery('137', '250', '0xtokenA', '0xtokenB', '10');
