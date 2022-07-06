3d8d9a1f240a206189f3c73f75bb436b9a3556cb
sandro <devspuig@pm.me>
2022-07-06 17:50:22 +0200

    Add BSC/USD price feed address

--- api/www/src/shared/PriceFeeds.ts
+++ api/www/src/shared/PriceFeeds.ts
@@ -1,4 +1,4 @@
-import { Fantom, Polygon } from './enums/ChainIds';
+import { BSC, Fantom, Polygon } from './enums/ChainIds';
 
 /**
  * These PriceFees are always from the native coin of the chain into USD,
@@ -7,4 +7,5 @@ import { Fantom, Polygon } from './enums/ChainIds';
 export const PriceFeeds = {
   [Polygon]: '0xAB594600376Ec9fD91F8e885dADF0CE036862dE0',
   [Fantom]: '0xf4766552D15AE4d256Ad41B6cf2933482B0680dc',
+  [BSC]: '0x0567F2323251f0Aab15c8dFb1967E4e8A7D42aeE',
 };
