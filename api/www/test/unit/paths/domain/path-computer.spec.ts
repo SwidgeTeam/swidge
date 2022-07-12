7157d4814eb5616eabd6660ec7ede3c89732ba40
sandro <devspuig@pm.me>
2022-07-12 11:43:04 +0200

    Add basic test for PathComputer

---
+++ api/www/test/unit/paths/domain/path-computer.spec.ts
@@ -0,0 +1,123 @@
+import { stub } from 'sinon';
+import { ZeroEx } from '../../../../src/swaps/domain/providers/zero-ex';
+import { SwapOrderComputer } from '../../../../src/swaps/application/query/swap-order-computer';
+import { PathComputer } from '../../../../src/paths/domain/path-computer';
+import { HttpClient } from '../../../../src/shared/http/httpClient';
+import { CachedHttpClient } from '../../../../src/shared/http/cachedHttpClient';
+import { BridgeOrderComputer } from '../../../../src/bridges/application/query/bridge-order-computer';
+import { TokenDetailsFetcher } from '../../../../src/shared/infrastructure/TokenDetailsFetcher';
+import { PriceFeedConverter } from '../../../../src/shared/infrastructure/PriceFeedConverter';
+import { GetPathQuery } from '../../../../src/paths/application/query/get-path.query';
+import { TokenMother } from '../../shared/domain/Token.mother';
+import { SwapOrder } from '../../../../src/swaps/domain/SwapOrder';
+import { ExchangeProviders } from '../../../../src/swaps/domain/providers/exchange-providers';
+import { BigInteger } from '../../../../src/shared/domain/BigInteger';
+import { BigNumber } from 'ethers';
+import { Multichain } from '../../../../src/bridges/domain/providers/multichain';
+import { BridgingOrder } from '../../../../src/bridges/domain/bridging-order';
+import { BridgingFees } from '../../../../src/bridges/domain/BridgingFees';
+import { BridgingLimits } from '../../../../src/bridges/domain/BridgingLimits';
+import { Tokens } from '../../../../src/shared/enums/Tokens';
+import { Fantom, Polygon } from '../../../../src/shared/enums/ChainIds';
+
+describe('path-computer', () => {
+  it('should compute a single path', async () => {
+    /** Arrange */
+    // Tokens
+    const srcToken = TokenMother.link();
+    const dstToken = TokenMother.sushi();
+    const bridgeTokenIn = Tokens.USDC[Polygon];
+    const bridgeTokenOut = Tokens.USDC[Fantom];
+
+    // mock TokenDetailsFetcher
+    const fetcher = new TokenDetailsFetcher();
+    const mockTokenFetcher = stub(fetcher, 'fetch');
+    mockTokenFetcher.onCall(0).resolves(srcToken);
+    mockTokenFetcher.onCall(1).resolves(dstToken);
+
+    // mock both calls of ZeroEx provider
+    const myHttpClient = HttpClient.create();
+    const zeroEx = new ZeroEx(myHttpClient);
+    const zeroExStub = stub(zeroEx, 'execute')
+      .onCall(0)
+      .resolves(
+        new SwapOrder(
+          ExchangeProviders.ZeroEx,
+          srcToken,
+          bridgeTokenIn,
+          '',
+          '',
+          BigInteger.fromDecimal('2', srcToken.decimals),
+          BigNumber.from('0'),
+          true,
+        ),
+      )
+      .onCall(1)
+      .resolves(
+        new SwapOrder(
+          ExchangeProviders.ZeroEx,
+          bridgeTokenOut,
+          dstToken,
+          '',
+          '',
+          BigInteger.fromDecimal('2', srcToken.decimals),
+          BigNumber.from('0'),
+          true,
+        ),
+      );
+    stub(zeroEx, 'isEnabledOn').returns(true);
+    stub(ZeroEx, 'create').returns(zeroEx);
+
+    // mock Multichain bridge provider
+    const myCachedHttpClient = CachedHttpClient.create();
+    const multichain = new Multichain(myCachedHttpClient);
+    const multichainStub = stub(multichain, 'execute')
+      .onCall(0)
+      .resolves(
+        new BridgingOrder(
+          BigInteger.zero(),
+          bridgeTokenIn,
+          bridgeTokenOut,
+          Fantom,
+          '',
+          new BridgingFees(0, BigInteger.fromDecimal('0'), BigInteger.fromDecimal('0'), 18),
+          new BridgingLimits(
+            BigInteger.fromDecimal('0'),
+            BigInteger.fromDecimal('0'),
+            BigInteger.fromDecimal('0'),
+            18,
+          ),
+          true,
+        ),
+      );
+    stub(multichain, 'isEnabledOn').returns(true);
+    stub(Multichain, 'create').returns(multichain);
+
+    // mock Chainlink's PriceFeedConverter
+    const priceFeedConverter = new PriceFeedConverter();
+    stub(priceFeedConverter, 'fetch').resolves(BigNumber.from('101010'));
+
+    // instantiate dependencies
+    const swapOrderComputer = new SwapOrderComputer(myHttpClient);
+    const bridgeOrderComputer = new BridgeOrderComputer(myHttpClient, myCachedHttpClient);
+
+    // create computer
+    const pathComputer = new PathComputer(
+      swapOrderComputer,
+      bridgeOrderComputer,
+      fetcher,
+      priceFeedConverter,
+    );
+
+    // create pat query
+    const query = new GetPathQuery(Polygon, Fantom, '0xLINK', '0xSUSHI', '1000000000000000000');
+
+    /** Act */
+    const path = await pathComputer.compute(query);
+
+    /** Assert */
+    expect(zeroExStub.callCount).toEqual(2);
+    expect(multichainStub.callCount).toEqual(1);
+    expect(path.destinationFee.toString()).toEqual('101010');
+  });
+});
