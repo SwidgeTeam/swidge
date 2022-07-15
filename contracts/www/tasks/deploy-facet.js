c6a118624e405f46db14038c9e9b920719d96023
sandro <devspuig@pm.me>
2022-07-15 16:40:23 +0200

    Comment the `deploy-facet.js` script

--- contracts/www/tasks/deploy-facet.js
+++ contracts/www/tasks/deploy-facet.js
@@ -9,23 +9,31 @@ module.exports = async function (taskArguments, hre, runSuper) {
   const allAddresses = getAddresses();
   const addresses = allAddresses[chain];
 
+  // deploy new facet
   const Facet = await hre.ethers.getContractFactory(facetName);
   const facet = await Facet.connect(deployer).deploy();
   await facet.deployed();
 
+  // get loupe handler
   const diamondLoupe = await hre.ethers.getContractAt(
     "IDiamondLoupe",
     addresses.diamond
   );
 
+  // get existing address of this facet
   const previousAddress = addresses.facet[facetName];
   const cuts = [];
 
+  // if the address exists means the facet it's already deployed,
+  // so we have to remove the selectors before we can instruct
+  // to add them again
   if (previousAddress) {
+    // fetch current selectors of the facet
     const previousSelectors = await diamondLoupe
       .connect(deployer)
       .facetFunctionSelectors(previousAddress);
 
+    // create the remove cut
     cuts.push({
       facetAddress: hre.ethers.constants.AddressZero,
       action: FacetCutAction.Remove,
@@ -33,20 +41,23 @@ module.exports = async function (taskArguments, hre, runSuper) {
     });
   }
 
-  // update facets
+  // create the addition cut
   cuts.push({
     facetAddress: facet.address,
     action: FacetCutAction.Add,
     functionSelectors: getSelectors(facet),
   });
 
+  // get cutter handler
   const diamondCutter = await hre.ethers.getContractAt(
     "IDiamondCutter",
     addresses.diamond
   );
 
+  // cut diamont
   (await diamondCutter.connect(deployer).diamondCut(cuts)).wait();
 
+  // save new facet address
   allAddresses[chain].facet[facetName] = facet.address;
   saveAddresses(allAddresses);
 };
