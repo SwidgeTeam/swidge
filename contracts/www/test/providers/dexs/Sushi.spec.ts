69d3df07e52be741d8c5923b2cfd6ca1d72179af
sandro <devspuig@pm.me>
2022-07-15 20:51:26 +0200

    Add basic tests on Sushi provider implementation

---
+++ contracts/www/test/providers/dexs/Sushi.spec.ts
@@ -0,0 +1,157 @@
+import chai, { expect } from "chai";
+import { FakeContract, smock } from "@defi-wonderland/smock";
+import { ethers } from "hardhat";
+import {
+  deployByName,
+  fakeTokenContract,
+  getAccounts,
+  RandomAddress,
+} from "../../shared";
+import { BigNumber, Contract } from "ethers";
+
+chai.use(smock.matchers);
+
+describe("Sushi provider", () => {
+  let contract: Contract;
+
+  beforeEach(async () => {
+    const { owner } = await getAccounts();
+    contract = await deployByName("Sushi", owner);
+  });
+
+  it("should revert if provider handler has no code", async () => {
+    // Arrange
+    const { owner } = await getAccounts();
+
+    // Act
+    const call = contract
+      .connect(owner)
+      .swap(RandomAddress, RandomAddress, RandomAddress, 0, "0x");
+
+    // Assert
+    expect(call).to.be.revertedWith("Provider has no code");
+  });
+
+  it("should fail when provider fails", async () => {
+    // Arrange
+    const { owner } = await getAccounts();
+    const sushiHandler = await fakeSushiSwapRouter();
+    const tokenIn = await fakeTokenContract();
+    const tokenOut = await fakeTokenContract();
+    const amountIn = BigNumber.from("10");
+    const callData = generateSwapExactTokensForTokensCallData([
+      100,
+      10,
+      [RandomAddress, RandomAddress],
+      RandomAddress,
+      100,
+    ]);
+
+    sushiHandler.swapExactTokensForTokens.reverts();
+
+    // Act
+    const call = contract
+      .connect(owner)
+      .swap(
+        sushiHandler.address,
+        tokenIn.address,
+        tokenOut.address,
+        amountIn,
+        callData
+      );
+
+    // Assert
+    await expect(call).to.be.revertedWith("Sushi failed: Reverted silently");
+  });
+
+  it("should not approve if native token in", async () => {});
+
+  it("should approve handler if non-native token in", async () => {
+    // Arrange
+    const { owner } = await getAccounts();
+    const sushiHandler = await fakeSushiSwapRouter();
+    const tokenIn = await fakeTokenContract();
+    const tokenOut = await fakeTokenContract();
+    const amountIn = BigNumber.from("10");
+    const callData = generateSwapExactTokensForTokensCallData([
+      100,
+      10,
+      [RandomAddress, RandomAddress],
+      RandomAddress,
+      100,
+    ]);
+
+    // Act
+    await contract
+      .connect(owner)
+      .swap(
+        sushiHandler.address,
+        tokenIn.address,
+        tokenOut.address,
+        amountIn,
+        callData
+      );
+
+    // Assert
+    expect(tokenIn.approve).to.be.calledOnceWith(
+      sushiHandler.address,
+      amountIn
+    );
+  });
+
+  it("should send correct callData to provider", async () => {
+    // Arrange
+    const { owner } = await getAccounts();
+    const sushiHandler = await fakeSushiSwapRouter();
+
+    const tokenIn = await fakeTokenContract();
+    const tokenOut = await fakeTokenContract({ balanceOf: 100 });
+    const amountIn = BigNumber.from("10");
+    const callData = generateSwapExactTokensForTokensCallData([
+      100,
+      10,
+      [RandomAddress, RandomAddress],
+      RandomAddress,
+      100,
+    ]);
+
+    // Act
+    await contract
+      .connect(owner)
+      .swap(
+        sushiHandler.address,
+        tokenIn.address,
+        tokenOut.address,
+        amountIn,
+        callData
+      );
+
+    // Assert
+    const encodedArgs = generateSwapExactTokensForTokensCallData(
+      <any[]>sushiHandler.swapExactTokensForTokens.getCall(0).args
+    );
+    expect(callData).to.be.contain(encodedArgs);
+  });
+});
+
+function generateSwapExactTokensForTokensCallData(args: any[]): string {
+  const selector = "0x38ed1739";
+  const encodedArgs = ethers.utils.defaultAbiCoder.encode(
+    ["uint256", "uint256", "address[]", "address", "uint256"],
+    args
+  );
+  return ethers.utils.hexConcat([selector, encodedArgs]);
+}
+
+async function fakeSushiSwapRouter(): Promise<FakeContract> {
+  const tokenAbi = [
+    "function swapExactTokensForTokens(" +
+      "      uint amountIn," +
+      "      uint amountOutMin," +
+      "      address[] calldata path," +
+      "      address to," +
+      "      uint deadline" +
+      "   ) external returns (uint[] memory amounts)",
+  ];
+  return await smock.fake(tokenAbi);
+}
--- contracts/www/test/shared.ts
+++ contracts/www/test/shared.ts
@@ -1,64 +1,70 @@
-import {FakeContract, smock} from "@defi-wonderland/smock";
-import {ethers} from "hardhat";
+import { FakeContract, smock } from "@defi-wonderland/smock";
+import { ethers } from "hardhat";
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+import { Signer } from 'ethers';
 
-export const NativeToken = '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE';
-export const ZeroAddress = '0x0000000000000000000000000000000000000000';
-export const RandomAddress = '0x1231231231231231321231231231231231231231';
-
+export const NativeToken = "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE";
+export const ZeroAddress = "0x0000000000000000000000000000000000000000";
+export const RandomAddress = "0x1231231231231231321231231231231231231231";
 
 export interface TokenContractResponses {
-    balanceOf?: boolean;
-    approve?: boolean;
-    transfer?: boolean;
-    transferFrom?: boolean;
+  balanceOf?: number;
+  approve?: boolean;
+  transfer?: boolean;
+  transferFrom?: boolean;
 }
 
 export async function getAccounts(): Promise<{
-    owner: SignerWithAddress;
-    anyoneElse: SignerWithAddress;
-    relayer: SignerWithAddress;
-    random: SignerWithAddress;
+  owner: SignerWithAddress;
+  anyoneElse: SignerWithAddress;
+  relayer: SignerWithAddress;
+  random: SignerWithAddress;
 }> {
-    const [owner, anyoneElse, relayer, random] = await ethers.getSigners();
-    return {
-        owner,
-        anyoneElse,
-        relayer,
-        random,
-    };
+  const [owner, anyoneElse, relayer, random] = await ethers.getSigners();
+  return {
+    owner,
+    anyoneElse,
+    relayer,
+    random,
+  };
 }
 
 /**
  * Instantiates a new ERC20 token fake contract
  * @param responses Allows to config the response values of the different methods
  */
-export async function fakeTokenContract(responses?: TokenContractResponses) {
-    const tokenAbi = [
-        "function name() view returns (string)",
-        "function symbol() view returns (string)",
-        "function totalSupply() view returns (uint256)",
-        "function balanceOf(address) view returns (uint)",
-        "function approve(address spender, uint256 value) returns (bool)",
-        "function transferFrom(address _from, address _to, uint256 _value) returns (bool)",
-        "function transfer(address _to, uint256 _value) returns (bool)",
-        "event Transfer(address indexed from, address indexed to, uint amount)"
-    ];
-    const fake = await smock.fake(tokenAbi);
+export async function fakeTokenContract(
+  responses?: TokenContractResponses
+): Promise<FakeContract> {
+  const tokenAbi = [
+    "function name() view returns (string)",
+    "function symbol() view returns (string)",
+    "function totalSupply() view returns (uint256)",
+    "function balanceOf(address) view returns (uint)",
+    "function approve(address spender, uint256 value) returns (bool)",
+    "function transferFrom(address _from, address _to, uint256 _value) returns (bool)",
+    "function transfer(address _to, uint256 _value) returns (bool)",
+    "event Transfer(address indexed from, address indexed to, uint amount)",
+  ];
+  const fake = await smock.fake(tokenAbi);
 
-    if (responses === undefined) responses = {};
+  if (responses === undefined) responses = {};
 
-    responses.balanceOf = responses.balanceOf === undefined ? true : responses.balanceOf;
-    responses.approve = responses.approve === undefined ? true : responses.approve;
-    responses.transfer = responses.transfer === undefined ? true : responses.transfer;
-    responses.transferFrom = responses.transferFrom === undefined ? true : responses.transferFrom;
+  responses.balanceOf =
+    responses.balanceOf === undefined ? 0 : responses.balanceOf;
+  responses.approve =
+    responses.approve === undefined ? true : responses.approve;
+  responses.transfer =
+    responses.transfer === undefined ? true : responses.transfer;
+  responses.transferFrom =
+    responses.transferFrom === undefined ? true : responses.transferFrom;
 
-    fake.balanceOf.returns(responses.balanceOf);
-    fake.approve.returns(responses.approve);
-    fake.transfer.returns(responses.transfer);
-    fake.transferFrom.returns(responses.transferFrom);
+  fake.balanceOf.returns(responses.balanceOf);
+  fake.approve.returns(responses.approve);
+  fake.transfer.returns(responses.transfer);
+  fake.transferFrom.returns(responses.transferFrom);
 
-    return fake;
+  return fake;
 }
 
 /**
@@ -68,28 +74,49 @@ export async function fakeTokenContract(responses?: TokenContractResponses) {
  * The rest of the bytes are the callData for that contract (not relevant on this scope)
  */
 export async function zeroExEncodedCalldata(): Promise<[string, FakeContract]> {
-    const ABI = ['function testFunction() external'];
-    const abiInterface = new ethers.utils.Interface(ABI);
-    const myFake = await smock.fake(ABI);
+  const ABI = ["function testFunction() external"];
+  const abiInterface = new ethers.utils.Interface(ABI);
+  const myFake = await smock.fake(ABI);
 
-    // Encode contract call address
-    const encodedCallAddress = ethers.utils.defaultAbiCoder.encode(
-        ['address'],
-        [myFake.address]
-    );
+  // Encode contract call address
+  const encodedCallAddress = ethers.utils.defaultAbiCoder.encode(
+    ["address"],
+    [myFake.address]
+  );
 
-    // Encode contract call payload
-    const encodedSelector = abiInterface.getSighash('testFunction');
-    const encodedArguments = ethers.utils.defaultAbiCoder.encode(
-        ['uint256', 'uint256'],
-        [10, 20]
-    );
+  // Encode contract call payload
+  const encodedSelector = abiInterface.getSighash("testFunction");
+  const encodedArguments = ethers.utils.defaultAbiCoder.encode(
+    ["uint256", "uint256"],
+    [10, 20]
+  );
 
-    // Pack bytes
-    const encodedCallData = ethers.utils.hexConcat([encodedSelector, encodedArguments]);
+  // Pack bytes
+  const encodedCallData = ethers.utils.hexConcat([
+    encodedSelector,
+    encodedArguments,
+  ]);
 
-    // Pack payload
-    const payload = ethers.utils.hexConcat([encodedCallAddress, encodedCallData]);
+  // Pack payload
+  const payload = ethers.utils.hexConcat([encodedCallAddress, encodedCallData]);
 
-    return [payload, myFake];
+  return [payload, myFake];
+}
+
+/**
+ * Deploy a specific contract by name
+ * @param contractName
+ * @param deployer
+ * @param args
+ * @returns {Promise<*>}
+ */
+export async function deployByName(
+  contractName: string,
+  deployer: Signer,
+  args = []
+) {
+  const Factory = await ethers.getContractFactory(contractName);
+  const contract = await Factory.connect(deployer).deploy(...args);
+  await contract.deployed();
+  return contract;
 }
