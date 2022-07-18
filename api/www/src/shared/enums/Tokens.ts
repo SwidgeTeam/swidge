2e6a3c4612aa2698e7cd6398283abf89f0d412b2
sandro <devspuig@pm.me>
2022-07-18 18:18:55 +0200

    Fix USDC token address for avalanche

--- api/www/src/shared/enums/Tokens.ts
+++ api/www/src/shared/enums/Tokens.ts
@@ -8,7 +8,7 @@ export const Tokens = {
     [Polygon]: new Token(USDC, '0x2791bca1f2de4661ed88a30c99a7a9449aa84174', 6),
     [Fantom]: new Token('USDCoin', '0x04068DA6C83AFCFA0e13ba15A6696662335D5B75', 6),
     [BSC]: new Token('USDCoin', '0x8ac76a51cc950d9822d68b83fe1ad97b32cd580d', 18),
-    [Avalanche]: new Token('USD Coin', '0xB97EF9Ef8734C71904D8002F8b6Bc66Dd9c48a6E', 6),
+    [Avalanche]: new Token('USD Coin', '0xA7D7079b0FEaD91F3e65f86E8915Cb59c1a4C664', 6),
     [Optimism]: new Token('USD Coin', '0x7f5c764cbc14f9669b88837ca1490cca17c31607', 6),
   },
 };
