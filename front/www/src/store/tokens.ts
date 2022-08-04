e43b6040dd6eb6113e89a1e7280ab57c7193226a
sandro <devspuig@pm.me>
2022-08-04 13:00:40 +0200

    Add token selection function to tokens store

--- front/www/src/store/tokens.ts
+++ front/www/src/store/tokens.ts
@@ -4,12 +4,23 @@ import IToken from '@/domain/tokens/IToken'
 
 export const useTokensStore = defineStore('tokens', {
     state: () => ({
-        tokens: [] as IToken[]
+        tokens: [] as IToken[],
+        originChainId: '',
+        originTokenAddress: '',
+        destinationChainId: '',
+        destinationTokenAddress: '',
     }),
     getters: {
+        /**
+         * Returns whole list of tokens
+         */
         getTokens(): IToken[] {
             return this.tokens
         },
+        /**
+         * Returns list of tokens of a specific chain
+         * @param state
+         */
         getChainTokens(state) {
             return (chainId: string): IToken[] => {
                 return state.tokens
@@ -17,11 +28,130 @@ export const useTokensStore = defineStore('tokens', {
                         return token.chainId === chainId
                     })
             }
+        },
+        /**
+         * Returns the selected origin chain ID
+         */
+        getOriginChainId(): string {
+            return this.originChainId
+        },
+        /**
+         * Returns the selected destination chain ID
+         */
+        getDestinationChainId(): string {
+            return this.destinationChainId
+        },
+        /**
+         * Returns the selected origin token address
+         */
+        getOriginTokenAddress(): string {
+            return this.originTokenAddress
+        },
+        /**
+         * Returns the selected destination token address
+         */
+        getDestinationTokenAddress(): string {
+            return this.destinationTokenAddress
+        },
+        /**
+         * Returns the selected origin chain name
+         */
+        getOriginChainName(): string {
+            const token = this.getOriginToken()
+            return token ? token.chainName : ''
+        },
+        /**
+         * Returns the selected destination chain name
+         */
+        getDestinationChainName(): string {
+            const token = this.getDestinationToken()
+            return token ? token.chainName : ''
+        },
+        /**
+         * Returns the selected origin token object
+         * @param state
+         */
+        getOriginToken(state) {
+            return (): IToken | undefined => {
+                return state.tokens
+                    .find(token => {
+                        return (
+                            token.chainId === state.originChainId &&
+                            token.address === state.originTokenAddress
+                        )
+                    })
+            }
+        },
+        /**
+         * Returns the selected destination token object
+         * @param state
+         */
+        getDestinationToken(state) {
+            return (): IToken | undefined => {
+                return state.tokens
+                    .find(token => {
+                        return (
+                            token.chainId === state.destinationChainId &&
+                            token.address === state.destinationTokenAddress
+                        )
+                    })
+            }
+        },
+        /**
+         * Returns whether both tokens are selected
+         */
+        bothTokensSelected(): boolean {
+            return (
+                this.originChainId !== '' &&
+                this.originTokenAddress !== '' &&
+                this.destinationChainId !== '' &&
+                this.destinationTokenAddress !== ''
+            )
+        },
+        /**
+         * Returns whether the tokens belong to the same chain
+         */
+        sameChainAssets(): boolean {
+            return this.originChainId === this.destinationChainId
         }
     },
     actions: {
+        /**
+         * Loads tokens list from API
+         */
         async fetchTokens() {
             this.tokens = await swidgeApi.fetchTokens()
+        },
+        /**
+         * Sets a specific token as selected on origin
+         * @param chainId
+         * @param address
+         */
+        selectOriginToken(chainId: string, address: string) {
+            this.originChainId = chainId
+            this.originTokenAddress = address
+        },
+        /**
+         * Selects a specific token as selected on destination
+         * @param chainId
+         * @param address
+         */
+        selectDestinationToken(chainId: string, address: string) {
+            this.destinationChainId = chainId
+            this.destinationTokenAddress = address
+        },
+        /**
+         * Switches origin and destination tokens one for the other
+         */
+        switchTokens() {
+            const auxChainId = this.originChainId
+            const auxAddress = this.originTokenAddress
+
+            this.originChainId = this.destinationChainId
+            this.originTokenAddress = this.destinationTokenAddress
+
+            this.destinationChainId = auxChainId
+            this.destinationTokenAddress = auxAddress
         }
     }
 })
