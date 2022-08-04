2db8f5b0e2ab6b06fc2056ab82e3ba72959a549c
sandro <devspuig@pm.me>
2022-08-04 22:37:49 +0200

    Add controller to update tokens price

---
+++ api/www/src/tokens/infrastructure/controllers/update-tokens-price-controller.ts
@@ -0,0 +1,19 @@
+import { Controller, Put, Res } from '@nestjs/common';
+import { CommandBus } from '@nestjs/cqrs';
+import { Response } from 'express';
+import { UpdateTokensPriceCommand } from '../../application/command/update-tokens-price-command';
+
+@Controller()
+export class AddTokensController {
+  constructor(private readonly commandBus: CommandBus) {}
+
+  @Put('update-tokens-price')
+  async updatePrice(@Res() res: Response) {
+    const command = new UpdateTokensPriceCommand();
+    await this.commandBus.execute<UpdateTokensPriceCommand, void>(command);
+
+    return res.json({
+      status: 'ok',
+    });
+  }
+}
