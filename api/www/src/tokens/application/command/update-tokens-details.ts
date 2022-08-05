82e3311ebf2d8bf64db0dafef695c9ce22420932
sandro <devspuig@pm.me>
2022-08-05 17:55:31 +0200

    Add pagination to tokens details update

--- api/www/src/tokens/application/command/update-tokens-details.ts
+++ api/www/src/tokens/application/command/update-tokens-details.ts
@@ -3,11 +3,14 @@ import { Class } from '../../../shared/Class';
 import { TokensRepository } from '../../domain/tokens.repository';
 import { Avalanche, BSC, Fantom, Optimism, Polygon } from '../../../shared/enums/ChainIds';
 import { Logger } from '../../../shared/domain/logger';
+import { TokenList } from '../../domain/TokenItem';
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const CoinGecko = require('coingecko-api');
 
 export class UpdateTokensDetails {
   private client;
+  private tokens: TokenList;
+  private COINS_PER_PAGE = 250;
 
   constructor(
     @Inject(Class.TokensRepository) private readonly repository: TokensRepository,
@@ -20,12 +23,27 @@ export class UpdateTokensDetails {
    * Entrypoint
    */
   async execute(): Promise<void> {
-    const list = await this.client.coins.all();
-    const tokens = await this.repository.getList();
+    this.tokens = await this.repository.getList();
+    let processedCoins,
+      page = 1;
+
+    do {
+      processedCoins = await this.checkNextPage(page);
+      page++;
+    } while (processedCoins === this.COINS_PER_PAGE);
+  }
+
+  async checkNextPage(page: number): Promise<number> {
+    this.logger.log(`page ${page}`);
+    const list = await this.client.coins.all({
+      per_page: this.COINS_PER_PAGE,
+      page: page,
+    });
 
     for (const coin of list.data) {
       await this.sleep(1); // for quota restrictions
       const coinId = coin.id;
+      this.logger.log(`checking ${coinId}`);
       const coin_data = await this.fetchCoinData(coinId);
       if (!coin_data.asset_platform_id) {
         // means it's a native coin, will treat them manually
@@ -36,15 +54,13 @@ export class UpdateTokensDetails {
           // convert chain id
           const chainId = this.getChainId(platform);
           // fetch token
-          const token = tokens.find(chainId, address as string);
+          const token = this.tokens.find(chainId, address as string);
           if (!token) {
             // if it doesn't exist means we don't support the token yet
             continue;
           }
           // update details
-          token
-            .setExternalId(coinId)
-            .setLogo(coin_data.image.small);
+          token.setExternalId(coinId).setLogo(coin_data.image.small);
           // update token
           this.repository.save(token);
           this.logger.log(`updated ${coinId}`);
@@ -54,6 +70,8 @@ export class UpdateTokensDetails {
         }
       }
     }
+
+    return list.data.length;
   }
 
   private async fetchCoinData(id: string) {
