115dd4ae6fb8b192380f8520fea5f947dc73aa14
sandro <devspuig@pm.me>
2022-08-05 23:37:22 +0200

    Complete service to update prices with special cases

--- api/www/src/tokens/application/command/update-tokens-price-handler.ts
+++ api/www/src/tokens/application/command/update-tokens-price-handler.ts
@@ -4,22 +4,13 @@ import { Class } from '../../../shared/Class';
 import { TokensRepository } from '../../domain/tokens.repository';
 import { UpdateTokensPriceCommand } from './update-tokens-price-command';
 import { TokenListItem } from '../../domain/TokenListItem';
-import {
-  AllChains,
-  Avalanche,
-  BSC,
-  Fantom,
-  Optimism,
-  Polygon,
-} from '../../../shared/enums/ChainIds';
+import { AllChains, Polygon } from '../../../shared/enums/ChainIds';
 import { TokenList } from '../../domain/TokenItem';
 import { CoinPriceFetcher } from '../../domain/coin-price-fetcher';
 import { TokensPriceFetcher } from '../../domain/tokens-price-fetcher';
 
 @CommandHandler(UpdateTokensPriceCommand)
 export class UpdateTokensPriceHandler implements ICommandHandler<UpdateTokensPriceCommand> {
-  private client;
-
   constructor(
     @Inject(Class.TokensRepository) private readonly repository: TokensRepository,
     @Inject(Class.CoinPriceFetcher) private readonly coinPriceFetcher: CoinPriceFetcher,
@@ -36,16 +27,29 @@ export class UpdateTokensPriceHandler implements ICommandHandler<UpdateTokensPri
     for (const chain of AllChains) {
       const chainTokens = tokens.ofChain(chain);
       if (chainTokens.count() > 0) {
-        await this.updateTokens(chainTokens, chain);
+        try {
+          await this.updateTokens(chainTokens, chain);
+        } catch (e) {
+          // log
+        }
       }
     }
 
     // update coins
-    for (const token of tokens.getNatives().items<TokenListItem[]>()) {
-      const price = await this.coinPriceFetcher.fetch(token.externalId);
-      token.setPrice(price);
-      this.repository.save(token);
+    const natives = tokens.getNatives();
+    for (const token of natives.items<TokenListItem[]>()) {
+      try {
+        const price = await this.coinPriceFetcher.fetch(token.externalId);
+        token.setPrice(price);
+        this.repository.save(token);
+      } catch (e) {
+        // log
+      }
     }
+
+    // special cases
+    const matic = natives.ofChain(Polygon).items()[0];
+    await this.updateMatic(matic);
   }
 
   /**
@@ -68,5 +72,19 @@ export class UpdateTokensPriceHandler implements ICommandHandler<UpdateTokensPri
     for (const token of tokens.items<TokenListItem[]>()) {
       this.repository.save(token);
     }
+  }
+
+  /**
+   * MATIC token has a specific address, thus it has to be quotes as
+   * a token instead of a coin, as ugly as it is, didn't have an easy option
+   * @param token
+   */
+  async updateMatic(token: TokenListItem) {
+    const prices = await this.tokensPriceFetcher.fetch(
+      ['0x0000000000000000000000000000000000001010'],
+      Polygon,
+    );
+    token.setPrice(prices[0].price);
+    this.repository.save(token);
   }
 }
