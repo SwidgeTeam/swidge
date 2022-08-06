67d7f081b8d4dbed6b0c7f95606d1d08e16c17f8
sandro <devspuig@pm.me>
2022-08-06 10:27:55 +0200

    Add and use Modal component

---
+++ front/www/src/components/Modal.vue
@@ -0,0 +1,66 @@
+<script setup lang='ts'>
+import { Dialog, DialogOverlay, TransitionChild, TransitionRoot } from '@headlessui/vue'
+import { XIcon } from '@heroicons/vue/solid'
+
+defineProps({
+    isOpen: {
+        type: Boolean,
+        default: true
+    },
+})
+
+const emits = defineEmits<{
+    (event: 'close'): void
+}>()
+
+const onCloseModal = () => {
+    emits('close')
+}
+</script>
+
+<template>
+    <TransitionRoot
+        as="template"
+        :show="isOpen"
+    >
+        <Dialog
+            as="div"
+            class="fixed inset-0 z-10 overflow-y-auto"
+            @close="onCloseModal()"
+        >
+            <div class="flex items-end justify-center min-h-screen px-4 pt-4 pb-20 text-center sm:block sm:p-0">
+                <TransitionChild
+                    as="template"
+                    enter="ease-out duration-300"
+                    enter-from="opacity-0"
+                    enter-to="opacity-100"
+                    leave="ease-in duration-200"
+                    leave-from="opacity-100"
+                    leave-to="opacity-0">
+                    <DialogOverlay class="fixed inset-0 transition-opacity bg-gray-500 bg-opacity-75"/>
+                </TransitionChild>
+                <span
+                    class="hidden sm:inline-block sm:align-middle sm:h-screen"
+                    aria-hidden="true">&#8203;</span>
+                <TransitionChild
+                    as="template"
+                    enter="ease-out duration-300"
+                    enter-from="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                    enter-to="opacity-100 translate-y-0 sm:scale-100"
+                    leave="ease-in duration-200"
+                    leave-from="opacity-100 translate-y-0 sm:scale-100"
+                    leave-to="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95">
+                    <div
+                        class="inline-block w-full max-w-xl px-10 py-12 text-left relative align-middle transition-all transform shadow-xl bg-[#222129] rounded-2xl"
+                    >
+                        <XIcon
+                            class="absolute w-5 top-6 right-6 cursor-pointer"
+                            @click="onCloseModal()"
+                        />
+                        <slot/>
+                    </div>
+                </TransitionChild>
+            </div>
+        </Dialog>
+    </TransitionRoot>
+</template>
