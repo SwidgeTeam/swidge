228dd4b734a496eef1b6846983f2a669cb3f54af
sandro <devspuig@pm.me>
2022-08-08 21:10:43 +0200

    Add rule ignore on vite.config.ts

--- front/www/vite.config.ts
+++ front/www/vite.config.ts
@@ -6,6 +6,8 @@ import Layouts from 'vite-plugin-vue-layouts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
     test: {
         globals: true,
         environment: 'happy-dom',
