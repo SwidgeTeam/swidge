b91106c2b5217f6eeb5445ec0111873aa2e425ed
sandro <devspuig@pm.me>
2022-08-17 22:17:11 +0200

    Add test for Rango provider

--- api/www/test/unit/aggregators/domain/providers/rango.spec.ts
+++ api/www/test/unit/aggregators/domain/providers/rango.spec.ts
@@ -7,31 +7,138 @@ import { QuoteRequest, QuoteResponse, RangoClient } from 'rango-sdk-basic';
 import { createMock } from 'ts-auto-mock';
 import { AggregatorProviders } from '../../../../../src/aggregators/domain/providers/aggregator-providers';
 
-jest.setTimeout(12000);
-
 describe('aggregators', () => {
-  it('should throw exception if request fails', async () => {
+  it('should throw exception resultType is NO_ROUTE', async () => {
     // Arrange
-    const rango = new Rango(new RangoClient('7b3d45e1-fdc1-4642-be95-3b8a8c6aebcf'));
-    const request = new AggregatorRequest(
-      '137',
-      '250',
-      TokenMother.polygonLink(),
-      TokenMother.fantomUsdc(),
-      BigInteger.fromDecimal('100'),
-      2,
-      faker.finance.ethereumAddress(),
-      faker.finance.ethereumAddress(),
-    );
+    const client = createMock<RangoClient>({
+      quote(quoteRequest: QuoteRequest): Promise<QuoteResponse> {
+        return Promise.resolve({
+          requestId: '',
+          resultType: 'NO_ROUTE',
+          route: null,
+        });
+      },
+    });
+    const rango = new Rango(client);
+    const request = getAggregatorRoute();
 
     // Act
-    const t1 = new Date().getTime();
-    const route = await rango.swap(request);
-    const t2 = new Date().getTime();
-    console.log(t2-t1);
-    //const route = await rango.meta();
+    const call = rango.execute(request);
 
     // Assert
+    await expect(call).rejects.toEqual(new Error('INSUFFICIENT_LIQUIDITY'));
+  });
+
+  it('should throw exception resultType is INPUT_LIMIT_ISSUE', async () => {
+    // Arrange
+    const client = createMock<RangoClient>({
+      quote(quoteRequest: QuoteRequest): Promise<QuoteResponse> {
+        return Promise.resolve({
+          requestId: '',
+          resultType: 'INPUT_LIMIT_ISSUE',
+          route: null,
+        });
+      },
+    });
+    const rango = new Rango(client);
+    const request = getAggregatorRoute();
+
+    // Act
+    const call = rango.execute(request);
+
+    // Assert
+    await expect(call).rejects.toEqual(new Error('INSUFFICIENT_LIQUIDITY'));
+  });
+
+  it('should throw exception resultType is HIGH_IMPACT', async () => {
+    // Arrange
+    const client = createMock<RangoClient>({
+      quote(quoteRequest: QuoteRequest): Promise<QuoteResponse> {
+        return Promise.resolve({
+          requestId: '',
+          resultType: 'HIGH_IMPACT',
+          route: null,
+        });
+      },
+    });
+    const rango = new Rango(client);
+    const request = getAggregatorRoute();
+
+    // Act
+    const call = rango.execute(request);
+
+    // Assert
+    await expect(call).rejects.toEqual(new Error('INSUFFICIENT_LIQUIDITY'));
+  });
+
+  it('should return a correct Route', async () => {
+    // Arrange
+    const client = createMock<RangoClient>({
+      quote(quoteRequest: QuoteRequest): Promise<QuoteResponse> {
+        return Promise.resolve({
+          requestId: '',
+          resultType: 'OK',
+          route: {
+            outputAmount: '18123456',
+            swapper: {
+              id: '',
+              title: '',
+              logo: '',
+            },
+            path: [
+              {
+                from: {
+                  blockchain: 'POLYGON',
+                  address: faker.finance.ethereumAddress(),
+                  symbol: 'SYM',
+                  decimals: 18,
+                  image: 'imgurl',
+                },
+                to: {
+                  blockchain: 'FANTOM',
+                  address: faker.finance.ethereumAddress(),
+                  symbol: 'SYM',
+                  decimals: 6,
+                  image: 'imgurl',
+                },
+                swapper: {
+                  id: '',
+                  title: '',
+                  logo: '',
+                },
+                swapperType: 'DEX',
+                expectedOutput: '18123456',
+              },
+            ],
+            fee: [
+              {
+                name: '',
+                token: {
+                  blockchain: 'FANTOM',
+                  address: faker.finance.ethereumAddress(),
+                  symbol: 'SYM',
+                  decimals: 6,
+                  image: 'imgurl',
+                },
+                expenseType: 'FROM_SOURCE_WALLET',
+                amount: '',
+              },
+            ],
+            amountRestriction: null,
+            estimatedTimeInSeconds: 600,
+          },
+        });
+      },
+    });
+    const rango = new Rango(client);
+    const request = getAggregatorRoute();
+
+    // Act
+    const route = await rango.execute(request);
+
+    // Assert
+    expect(route.aggregator.id).toEqual('4');
+    expect(route.amountOut.toString()).toEqual('18.123456');
   });
 });
 
