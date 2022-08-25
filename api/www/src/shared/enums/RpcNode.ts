e92f07d39732c9e362d90a1d70d4105243f89202
sandro <devspuig@pm.me>
2022-08-25 19:28:40 +0200

    Add Mainnet RPC entries

--- api/www/src/shared/enums/RpcNode.ts
+++ api/www/src/shared/enums/RpcNode.ts
@@ -1,7 +1,8 @@
-import { Fantom, Polygon, BSC, Avalanche, Optimism } from './ChainIds';
+import { Fantom, Polygon, BSC, Avalanche, Optimism, Mainnet } from './ChainIds';
 import 'dotenv/config';
 
 export const RpcNode = {
+  [Mainnet]: process.env.RPC_NODE_MAINNET,
   [Polygon]: process.env.RPC_NODE_POLYGON,
   [Fantom]: process.env.RPC_NODE_FANTOM,
   [BSC]: process.env.RPC_NODE_BSC,
