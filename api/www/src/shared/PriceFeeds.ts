78dd4b007218eb303dcd8aed66292d708025230f
sandro <devspuig@pm.me>
2022-08-25 19:04:51 +0200

    Add Chainlink's ETH/USD price feed

--- api/www/src/shared/PriceFeeds.ts
+++ api/www/src/shared/PriceFeeds.ts
@@ -1,10 +1,11 @@
-import { Avalanche, BSC, Fantom, Optimism, Polygon } from './enums/ChainIds';
+import { Avalanche, BSC, Fantom, Mainnet, Optimism, Polygon } from './enums/ChainIds';
 
 /**
  * These PriceFees are always from the native coin of the chain into USD,
  * so we can convert from one native to the other by using two of them
  */
 export const PriceFeeds = {
+  [Mainnet]: '0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419',
   [Polygon]: '0xAB594600376Ec9fD91F8e885dADF0CE036862dE0',
   [Fantom]: '0xf4766552D15AE4d256Ad41B6cf2933482B0680dc',
   [BSC]: '0x0567F2323251f0Aab15c8dFb1967E4e8A7D42aeE',
