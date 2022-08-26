f6be47ca2075e3b36b3f14770f6000f369caa902
sandro <devspuig@pm.me>
2022-08-26 23:49:56 +0200

    Install dependencies for WalletConnect and add hack to use it

--- front/www/vite.config.ts
+++ front/www/vite.config.ts
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
 import Pages from 'vite-plugin-pages'
 import Layouts from 'vite-plugin-vue-layouts'
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -27,6 +28,18 @@ export default defineConfig({
     optimizeDeps: {
         include: ['vue', 'vue-router', '@vueuse/core', '@vueuse/head'],
         exclude: ['vue-demi'],
+        esbuildOptions: {
+            // Node.js global to browser globalThis
+            define: {
+                global: 'globalThis'
+            },
+            // Enable esbuild polyfill plugins
+            plugins: [
+                NodeGlobalsPolyfillPlugin({
+                    buffer: true
+                })
+            ]
+        }
     },
     server: {
         hmr: true,
