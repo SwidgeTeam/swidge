d3f525c32cab3dea09788b0ce62a1f09e6f6fdb1
ArpitaGanatra <ganatraarpita@gmail.com>
2022-09-02 17:35:04 +0530

    added receiver address to store

--- front/www/src/components/RecipientUserCard.vue
+++ front/www/src/components/RecipientUserCard.vue
@@ -1,78 +1,65 @@
 <script setup lang="ts">
-import { useWeb3Store } from '@/store/web3'
-import { storeToRefs } from 'pinia'
-import { useTokensStore } from '@/store/tokens'
-import { Networks } from '@/domain/chains/Networks'
-import { ref } from 'vue';
-
-const web3Store = useWeb3Store()
-const { account, isConnected, isCorrectNetwork, selectedNetworkId } =
-    storeToRefs(web3Store)
-console.log(account, isConnected, isCorrectNetwork, selectedNetworkId)
-
-const tokensStore = useTokensStore()
-
-const props = defineProps<{
-    disabledInput: boolean
-}>()
-
-const isExpanded = ref(false);
-
-const toggleLable = () => {
-    return isExpanded.value ? 'show less' : 'show more'
-}
-const getTokenLogo = (): string => {
-    const token = tokensStore.getDestinationToken()
-    return token ? token.logo : ''
-}
-
-const onFallbackImgHandler = (e: Event) => {
-    const token = tokensStore.getDestinationToken()
-    if (token) {
-        const chain = Networks.get(token.chainId)
-        const imageTarget = e.target as HTMLImageElement
-        imageTarget.src = chain.icon
+    import { useWeb3Store } from '@/store/web3'
+    import { storeToRefs } from 'pinia'
+    import { useTokensStore } from '@/store/tokens'
+    import { useRoutesStore } from '@/store/routes'
+    import { Networks } from '@/domain/chains/Networks'
+    
+    const web3Store = useWeb3Store()
+    const routesStore = useRoutesStore()
+    const { account, isConnected} = storeToRefs(web3Store)
+    const { receiverAddress } = storeToRefs(routesStore)
+    routesStore.setReceiverAddress(account.value)
+    
+    const tokensStore = useTokensStore()
+    
+    const props = defineProps<{
+        disabledInput: boolean
+    }>()
+    
+    const getTokenLogo = (): string => {
+        const token = tokensStore.getDestinationToken()
+        return token ? token.logo : ''
     }
-}
-
-const createShortAddress = (address: string, visibleAddress:number): string => {
-    return address.substring(0, visibleAddress) + '...'
-}
-
+    
+    const onFallbackImgHandler = (e: Event) => {
+        const token = tokensStore.getDestinationToken()
+        if (token) {
+            const chain = Networks.get(token.chainId)
+            const imageTarget = e.target as HTMLImageElement
+            imageTarget.src = chain.icon
+        }
+    }
+    
+    const onAccountChange = (event: Event) => {
+        if (!(event.target instanceof HTMLInputElement)) return
+        const value = event.target.value
+        routesStore.setReceiverAddress(value)
+    }
+    
 </script>
 
 <template>
-    <div
-        class="flex flex-col gap-2 px-4 py-3 items-center border-[#626060]/40 border rounded-2xl w-[32rem]"
-    >
-        <div class="flex justify-between w-full">
-            <div class=" font-light flex flex-85 w-full">
-                <div v-if="isConnected" class="flex gap-2 flex-wrap w-full ">
-                <img v-if="tokensStore.getDestinationToken()"
+    <div class="flex align-center justify-center px-4  border-[#626060]/40 border rounded-2xl w-[32rem]">
+        <div v-if="isConnected" class="flex w-full">
+            <div class="self-center" v-if="tokensStore.getDestinationToken()">
+                <img 
                     :src="getTokenLogo()"
                     class="rounded-full"
                     width="24"
                     height="24"
                     @error="onFallbackImgHandler"
                 />
-                <!-- <span class="" v-if="isExpanded">{{account}}</span> -->
-               <input
-               v-if="isExpanded"
-                    type="text"
-                    :disabled="disabledInput"
-                    :placeholder=account
-                    class="w-full p-0 text-left text-white placeholder-white bg-transparent border-none outline-none appearance-none focus:border-transparent focus:ring-0"
-                    autocomplete="off"
-                    minlength="1"
-                    maxlength="79"
-                />
-                <span v-else>{{ createShortAddress(account, 3) }}</span>
-                 
-                <button class="underline flex-none flex-nowrap" @click="isExpanded = !isExpanded">{{toggleLable()}}</button>
-                </div>
-                <span v-else>Recipient Address</span>
             </div>
-            <div class="flex flex-15">User.user</div>
+            <input
+                type="text"
+                class="w-full py-3 bg-transparent text-center border-none outline-none appearance-none focus:border-transparent focus:ring-0 truncate"
+                :value=receiverAddress
+                minlength="1"
+                maxlength="79"
+                @change=onAccountChange
+            />
         </div>
+        <span v-else class="py-3">Recipient Address</span>
     </div>
 </template>
--- front/www/src/store/routes.ts
+++ front/www/src/store/routes.ts
@@ -12,6 +12,7 @@ export const useRoutesStore = defineStore('routes', {
         selectedRoute: 0,
         slippageValue: '2',
         gasPriority: 'medium',
+        receiverAddress: ''
     }),
     getters: {
         getSelectedRoute(): Route {
@@ -22,6 +23,9 @@ export const useRoutesStore = defineStore('routes', {
         },
         getGasPriority(): string {
             return this.gasPriority
+        },
+        getReceiverAddress(): string {
+            return this.receiverAddress
         },
     },
     actions: {
@@ -41,9 +45,9 @@ export const useRoutesStore = defineStore('routes', {
                 amount: amount,
                 slippage: Number(routesStore.getSlippage),
                 senderAddress: web3Store.account || ethers.constants.AddressZero,
-                receiverAddress: web3Store.account || ethers.constants.AddressZero
+                receiverAddress: routesStore.getReceiverAddress
             })
-            this.selectRoute(0) // selects the top route
+            this.selectRoute(0) // selects the top route       
         },
         /**
          * Marks the route `index` as selected
@@ -85,7 +89,10 @@ export const useRoutesStore = defineStore('routes', {
          */
         setGasPriority(value: string) {
             this.gasPriority = value
-        }
+        },
+        setReceiverAddress(value: string) {
+            this.receiverAddress = value
+        },
     }
 })
 
