2e67bd4ad5e9fa4016f99376402d00a561b501d3
sandro <devspuig@pm.me>
2022-09-04 21:59:11 +0200

    Add wallet icon

--- front/www/src/components/Buttons/AddressButton.vue
+++ front/www/src/components/Buttons/AddressButton.vue
@@ -1,4 +1,6 @@
 <script setup lang='ts'>
+import WalletIcon from '@/components/svg/WalletIcon.vue'
+
 defineProps<{
     address: string
 }>()
@@ -22,6 +24,7 @@ const createShorterAddress = (address: string): string => {
         class="header-button"
         @click="emits('click')"
     >
+        <WalletIcon/>
         <span class="hidden sm:block">{{ createShortAddress(address) }}</span>
         <span class="block sm:hidden">{{ createShorterAddress(address) }}</span>
     </button>
