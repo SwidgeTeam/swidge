4a8fcaf1f3bca09d185f567e8fa7674fa5cc9c37
sandro <devspuig@pm.me>
2022-09-05 08:34:18 +0200

    Hide recipient until integration with new design

--- front/www/src/components/SwapBox.vue
+++ front/www/src/components/SwapBox.vue
@@ -5,7 +5,6 @@ import TransactionDetails from './TransactionDetails.vue'
 import BridgeSwapInteractiveButton from './BridgeSwapInteractiveButton.vue'
 import AdjustmentsIcon from './svg/AdjustmentIcon.vue'
 import ModalSettings from './Modals/ModalSettings.vue'
-import RecipientUserCard from './RecipientUserCard.vue'
 
 defineProps<{
     sourceTokenAmount: string
@@ -35,12 +34,12 @@ const emits = defineEmits<{
         <div class="flex flex-col w-full gap-4">
             <span class="relative text-2xl px-4">
                 You send:
-                    <AdjustmentsIcon 
+                    <AdjustmentsIcon
                         class="absolute w-10 h-8 right-4 top-0 cursor-pointer"
                         @click="isSettingsModalOpen = true"
                     />
             </span>
-            
+
             <BridgeSwapSelectionCard
                 :value="sourceTokenAmount"
                 :is-origin="true"
@@ -63,9 +62,6 @@ const emits = defineEmits<{
                 @open-token-list="() => emits('select-destination-token')"
             />
         </div>
-        <div>
-            <RecipientUserCard />
-        </div>
         <TransactionDetails
             v-if="transactionFees"
             :total-fee="transactionFees"
@@ -76,7 +72,7 @@ const emits = defineEmits<{
             :disabled="isExecuteButtonDisabled"
             :on-click="() => emits('execute-transaction')"
         />
-        <ModalSettings 
+        <ModalSettings
         :is-open="isSettingsModalOpen"
         @close-modal="isSettingsModalOpen = false"
         />
