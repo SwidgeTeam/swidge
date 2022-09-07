e3ac6f171e0d6beb86979fcbb613a9f7a2340d71
sandro <devspuig@pm.me>
2022-09-07 09:41:51 +0200

    Remove duplicated

--- api/www/src/aggregators/domain/providers/liFi.ts
+++ api/www/src/aggregators/domain/providers/liFi.ts
@@ -40,8 +40,6 @@ import {
   Polygon,
   xDAI,
 } from '../../../shared/enums/ChainIds';
-import { ethers } from 'ethers';
-import { NATIVE_TOKEN_ADDRESS } from '../../../shared/enums/Natives';
 
 export class LiFi implements Aggregator, ExternalAggregator, MetadataProviderAggregator {
   private enabledChains = [
