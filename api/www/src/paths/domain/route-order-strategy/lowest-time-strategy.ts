aa1f688e687b48848c68cf87d2fc60c4ff5b420c
sandro <devspuig@pm.me>
2022-09-08 18:49:18 +0200

    Fix correct order on lowest time order strategy

--- api/www/src/paths/domain/route-order-strategy/lowest-time-strategy.ts
+++ api/www/src/paths/domain/route-order-strategy/lowest-time-strategy.ts
@@ -1,15 +1,16 @@
 import { IOrderStrategy } from './order-strategy';
 import { Route } from '../../../shared/domain/route/route';
-import descendingOrder from '../../../shared/domain/descending-order';
-import { BigInteger } from '../../../shared/domain/big-integer';
 
 export class LowestTimeStrategy implements IOrderStrategy {
   order(routes: Route[]): Route[] {
     return routes.sort((a, b) => {
-      return descendingOrder(
-        BigInteger.fromDecimal(a.resume.estimatedTime.toString()),
-        BigInteger.fromDecimal(b.resume.estimatedTime.toString()),
-      );
+      if (a.resume.estimatedTime < b.resume.estimatedTime) {
+        return -1;
+      } else if (a.resume.estimatedTime > b.resume.estimatedTime) {
+        return 1;
+      } else {
+        return 0;
+      }
     });
   }
 }
