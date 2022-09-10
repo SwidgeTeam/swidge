f2310fdc583f6f38d35f33bac68176f603ba6e1e
Fabbaist <WeberFabian1@gmx.de>
2022-09-10 09:12:45 +0100

    Add Discord/Twitter SVG Logos and Import to COming Soon page

--- front/www/src/components/ComingSoon.vue
+++ front/www/src/components/ComingSoon.vue
@@ -1,6 +1,13 @@
+<script setup lang="ts">
+import TwitterLogo from '@/components/svg/TwitterLogo.vue'
+import DiscordLogo from '@/components/svg/DiscordLogo.vue'
+</script>
+
 <template>
-    <h1 class="text-white font-extrabold flex flex-col text-center">
-        Coming Soon! 
-        Subscribe to our Twitter or join our Discord to stay updated. 
-    </h1>
+   <h1 class="text-white-500 font-extrabold flex flex-col text-center">
+    <span class="text-7xl">Coming Soon!</span>
+    <span class="text-5xl mt-5">Subscribe to our or join our Discord to stay updated.</span>
+    <TwitterLogo class="w-10 h-10 itmes-center align-center"></TwitterLogo>
+    <DiscordLogo></DiscordLogo>
+  </h1>
 </template>
