763aca5466f56fab776b6fba3848289864ea328e
sandro <devspuig@pm.me>
2022-09-11 10:40:01 +0200

    Add cache for metadata

--- api/www/src/metadata/application/query/get-metadata-handler.ts
+++ api/www/src/metadata/application/query/get-metadata-handler.ts
@@ -13,24 +13,62 @@ import { ConfigService } from '../../../config/config.service';
 @QueryHandler(GetMetadataQuery)
 export class GetMetadataHandler implements IQueryHandler<GetMetadataQuery> {
   private aggregators: Map<string, MetadataProviderAggregator>;
+  private cache: Metadata | undefined;
+  private deadline: number | undefined;
+  private readonly CACHE_TIME = 60 * 60 * 1000; // 1 hour
 
   constructor(
     private readonly configService: ConfigService,
     @Inject(Class.Logger) private readonly logger: Logger,
   ) {
     this.aggregators = new Map<string, MetadataProviderAggregator>([
-      [AggregatorProviders.LiFi, LiFi.create(logger)],
       [AggregatorProviders.Rango, Rango.create(configService.getRangoApiKey(), logger)],
+      [AggregatorProviders.LiFi, LiFi.create(logger)],
     ]);
   }
 
   async execute(): Promise<Metadata> {
     this.logger.log('Fetching metadata...');
+
+    if (this.isCached()) {
+      this.logger.log('Cached meta');
+      return this.cache;
+    }
+
     const metadata = new Metadata();
+    let isEligibleForCache = true;
     for (const aggregator of this.aggregators.values()) {
       const meta = await aggregator.getMetadata();
       metadata.includeAggregatorMetadata(meta);
+      isEligibleForCache =
+        isEligibleForCache && meta.chains.length !== 0 && meta.tokens.length !== 0;
     }
+
+    if (isEligibleForCache) {
+      this.logger.log('Stored meta on cache');
+      this.cache = metadata;
+      this.deadline = this.getDeadline();
+    }
+
     return metadata;
+  }
+
+  /**
+   * Checks if result is cached and still valid
+   * @private
+   */
+  private isCached(): boolean {
+    if (!this.cache) {
+      return false;
+    }
+    return new Date().getTime() < this.deadline;
+  }
+
+  /**
+   * Computes the deadline given the current time
+   * @private
+   */
+  private getDeadline(): number {
+    return new Date().getTime() + this.CACHE_TIME;
   }
 }
