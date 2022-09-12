ce62bd6c3b0a2bc72b08d10fcf558c8636747f37
Fabbaist <WeberFabian1@gmx.de>
2022-09-13 00:43:11 +0200

    added social links and design to Coming Soon page

--- front/www/src/components/ComingSoon.vue
+++ front/www/src/components/ComingSoon.vue
@@ -5,9 +5,15 @@ import DiscordLogo from '@/components/svg/DiscordLogo.vue'
 
 <template>
    <h1 class="text-white-500 font-extrabold flex flex-col text-center">
-    <span class="text-7xl">Coming Soon!</span>
-    <span class="text-5xl mt-5">Subscribe to our or join our Discord to stay updated.</span>
-    <TwitterLogo class="w-10 h-10 itmes-center align-center"></TwitterLogo>
-    <DiscordLogo></DiscordLogo>
+    <span class="text-5xl">Coming Soon!</span>
+    <span class="text-3xl mt-5 mb-10">Follow us on Twitter and join our Discord to stay updated.</span>
+    <div class="flex flex-auto place-content-center items-center align-center">
+      <a href="https://twitter.com/therealswidge">
+        <TwitterLogo class="w-10 h-10 itmes-center align-center"></TwitterLogo>
+      </a>
+      <a href="https://discord.swidge.xyz/">
+        <DiscordLogo class="w-10 h-10 items-center align-center" ></DiscordLogo>
+      </a>
+  </div>
   </h1>
 </template>
