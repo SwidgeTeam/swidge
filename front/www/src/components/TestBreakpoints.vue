52eaa2a8d757bd4e096c696a572ac25e550b1b54
sandro <devspuig@pm.me>
2022-09-13 08:57:16 +0200

    Add component to test breakpoints on dev process

---
+++ front/www/src/components/TestBreakpoints.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="fixed z-50 text-xs font-bold text-black p-2">
+        <span class="inline-block xs:hidden rounded px-1 bg-yellow-400 p-1">xXS</span>
+        <span class="hidden xs:inline-block sm:hidden rounded px-1 bg-yellow-400 p-1">XS</span>
+        <span class="hidden sm:inline-block md:hidden rounded px-1 bg-yellow-400">SM</span>
+        <span class="hidden sm:hidden md:inline-block lg:hidden rounded px-1 bg-yellow-400">MD</span>
+        <span class="hidden lg:inline-block xl:hidden rounded px-1 bg-yellow-400">LG</span>
+        <span class="hidden xl:inline-block rounded px-1 bg-yellow-400">XL</span>
+    </div>
+</template>
