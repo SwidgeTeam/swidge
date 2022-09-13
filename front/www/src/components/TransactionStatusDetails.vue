3d1d2008db269227c91c305b88fbc43dcc2f97ba
sandro <devspuig@pm.me>
2022-09-13 10:15:33 +0200

    Fix use of TokenLogo

--- front/www/src/components/TransactionStatusDetails.vue
+++ front/www/src/components/TransactionStatusDetails.vue
@@ -26,7 +26,10 @@ const fixedAmount = (amount: number) => {
             class='tooltip rounded-xl shadow-lg p-1 bg-[#31313E] text-white text-sm font-light absolute border border-cyan-700 -bottom-6 -left-20 px-2'>
             {{ tokenName }}
         </span>
-            <TokenLogo :logo="tokenLogo" size="64"/>
+            <TokenLogo
+                :token-logo="tokenLogo"
+                :chain-logo="chainLogo"
+                size="64"/>
             <ChainLogo :logo="chainLogo" size="48"/>
     </div>
     <div
