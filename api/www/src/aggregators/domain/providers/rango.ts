25fbdb38f05ddfa9a9f87df3a8d126b493390041
sandro <devspuig@pm.me>
2022-09-23 12:16:04 +0200

    Hardcode gasLimit when Rango sends null

--- api/www/src/aggregators/domain/providers/rango.ts
+++ api/www/src/aggregators/domain/providers/rango.ts
@@ -259,7 +259,7 @@ export class Rango
       tx.txTo,
       tx.txData,
       tx.value ? BigInteger.fromString(tx.value) : BigInteger.zero(),
-      tx.gasLimit ? BigInteger.fromString(tx.gasLimit) : BigInteger.zero(),
+      tx.gasLimit ? BigInteger.fromString(tx.gasLimit) : BigInteger.fromString('600000'),
     );
 
     return new BothTxs(response.requestId, approvalTx, mainTx);
