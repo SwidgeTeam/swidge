6cae95d4a00bfb56c6d937fa9a993f96863b84bb
Fabbaist <WeberFabian1@gmx.de>
2022-09-27 09:52:02 +0100

    header spacing wallet selection fixed

--- front/www/src/components/Modals/ModalWallets.vue
+++ front/www/src/components/Modals/ModalWallets.vue
@@ -74,21 +74,21 @@ const getWallets = () => {
                     leave-to="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"> 
                     <div
                         class="flex sm:inline-block self-center align-center justify-self-auto 
-                        justify-center w-fit w-50 sm:max-w-md px-8 py-2 sm:px-2
-                     sm:py-2 text-left relative transition-all transform shadow-xl 
+                        justify-center w-fit w-50 sm:max-w-md px-8 py-4 sm:px-2
+                     sm:py-4 text-left relative transition-all transform shadow-xl 
                      bg-[#222129] radial-gradient rounded-2xl"
                     > 
                     <XIcon
-                            class="absolute w-5 top-2 right-2 sm:top-6 sm:right-6 cursor-pointer"
+                            class="absolute w-5 top-3 right-3 sm:top-4 sm:right-6 cursor-pointer"
                             @click="onCloseModal()"
                         />
-                    <h1 class="absolute w-5 top-3 left-6 sm:top-6 sm:right-6 cursor-pointer font-semibold font-roboto text-xl">Connect</h1>
+                    <h1 class="absolute w-5 mb- top-3 left-6 sm:top-4 sm:left-6 cursor-pointer font-semibold font-roboto text-xl">Connect</h1>
                     <div class="flex flex-col sm:flex-row py-4 px-2 justify-center">
                         
                         <button
                             v-for="(wallet, index) in getWallets()"
                             :key="index"
-                            class="inline-block justify-center w-fit px-3 pt-8 m-5 font-roboto font-semibold 
+                            class="inline-block align-center items-center justify-center w-fit px-3 py-2 m-5 font-roboto font-semibold 
                              hover:gradient-border-header-main-hover-wallet"
                             @click="selectWallet(wallet.key)"
                         >
