cc876edaa39aadb0dcbf6f60f8ea62729856e34c
Fabbaist <WeberFabian1@gmx.de>
2022-09-27 09:25:51 +0100

    Change Wallet Button Colour and hover behaviour, adapt padding

--- front/www/src/components/Modals/ModalWallets.vue
+++ front/www/src/components/Modals/ModalWallets.vue
@@ -73,7 +73,8 @@ const getWallets = () => {
                     leave-from="opacity-100 translate-y-0 sm:scale-100"
                     leave-to="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"> 
                     <div
-                        class="flex sm:inline-block self-center align-center justify-self-auto justify-center w-full max-w-sm sm:max-w-xl px-2 py-8 sm:px-
+                        class="flex sm:inline-block self-center align-center justify-self-auto 
+                        justify-center w-fit w-50 sm:max-w-md px-8 py-2 sm:px-2
                      sm:py-2 text-left relative transition-all transform shadow-xl 
                      bg-[#222129] radial-gradient rounded-2xl"
                     > 
@@ -82,12 +83,12 @@ const getWallets = () => {
                             @click="onCloseModal()"
                         />
                     <h1 class="absolute w-5 top-3 left-6 sm:top-6 sm:right-6 cursor-pointer font-semibold font-roboto text-xl">Connect</h1>
-                    <div class="flex flex-col sm:flex-row py-10 px-8 justify-center">
+                    <div class="flex flex-col sm:flex-row py-4 px-2 justify-center">
                         <button
                             v-for="(wallet, index) in getWallets()"
                             :key="index"
-                            class="inline-block justify-center w-fit px-7 py-5 m-5 font-roboto font-semibold 
-                            gradient-border-header-main hover:gradient-border-header-main-hover"
+                            class="inline-block justify-center w-fit px-3 pt-8 m-5 font-roboto font-semibold 
+                             hover:gradient-border-header-main-hover-wallet"
                             @click="selectWallet(wallet.key)"
                         >
                             <img 
