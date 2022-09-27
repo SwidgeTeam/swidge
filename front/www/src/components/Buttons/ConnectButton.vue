147a50536f888223827a8d9e7e63418fc3cb5095
Fabbaist <WeberFabian1@gmx.de>
2022-09-27 10:49:18 +0100

    Changed HeaderMain Button Styles

--- front/www/src/components/Buttons/ConnectButton.vue
+++ front/www/src/components/Buttons/ConnectButton.vue
@@ -9,7 +9,15 @@ const emits = defineEmits<{
 
 <template>
     <button
-        class="header-button"
+        class="flex
+        items-center
+        gap-2
+        tracking-wide
+        px-3 py-1
+        font-roboto
+        font-light
+        gradient-border-header-main
+        hover:gradient-border-header-main-hover"
         @click="emits('connect')"
     >
         <LinkIcon class="h-5"/>
