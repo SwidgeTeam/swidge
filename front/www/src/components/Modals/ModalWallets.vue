36df8010ecd8295f827e899566576cbfcd96bb8f
sandro <devspuig@pm.me>
2022-09-30 20:55:37 +0200

    Create basic component for the account popover

--- front/www/src/components/Modals/ModalWallets.vue
+++ front/www/src/components/Modals/ModalWallets.vue
@@ -71,39 +71,39 @@ const getWallets = () => {
                     enter-to="opacity-100 translate-y-0 sm:scale-100"
                     leave="ease-in duration-200"
                     leave-from="opacity-100 translate-y-0 sm:scale-100"
-                    leave-to="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"> 
+                    leave-to="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95">
                     <div
-                        class="flex sm:inline-block self-center align-center justify-self-auto 
+                        class="flex sm:inline-block self-center align-center justify-self-auto
                         justify-center w-fit w-50 sm:max-w-md px-8 py-4 sm:px-2
-                     sm:py-4 text-left relative transition-all transform shadow-xl 
-                     bg-[#222129] radial-gradient rounded-2xl gradient-border-header-main-hover"
-                    > 
+                     sm:py-4 text-left relative transition-all transform shadow-xl
+                     bg-[#222129] wallets-bg-gradient rounded-2xl gradient-border-header-main-hover"
+                    >
                     <XIcon
                             class="absolute w-5 top-3 right-3 sm:top-4 sm:right-6 cursor-pointer"
                             @click="onCloseModal()"
                         />
                     <h1 class="absolute w-5 mb- top-3 left-6 sm:top-4 sm:left-6 cursor-pointer font-semibold font-roboto text-xl">Connect</h1>
                     <div class="flex flex-col sm:flex-row py-4 px-2 justify-center">
-                        
+
                         <button
                             v-for="(wallet, index) in getWallets()"
                             :key="index"
-                            class="inline-block align-center items-center justify-center w-fit px-3 py-2 m-5 font-roboto font-semibold 
+                            class="inline-block align-center items-center justify-center w-fit px-3 py-2 m-5 font-roboto font-semibold
                              hover:gradient-border-header-main-hover-wallet"
                             @click="selectWallet(wallet.key)"
                         >
-                            <img 
+                            <img
                                 class ="max-w-sm object-contain w-32 h-24"
                                 :alt="wallet.name + ' icon'"
                                 :src="wallet.icon"/>
                             <span>{{ wallet.name }}</span>
                         </button>
-                        
-                    </div>                     
+
+                    </div>
                     </div>
                 </TransitionChild>
             </div>
         </Dialog>
     </TransitionRoot>
-    
+
 </template>
