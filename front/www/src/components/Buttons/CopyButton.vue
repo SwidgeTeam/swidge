4e755fcc299f398737d9392b447bfff6f22bf692
sandro <devspuig@pm.me>
2022-09-30 23:26:47 +0200

    Add CopyButton

---
+++ front/www/src/components/Buttons/CopyButton.vue
@@ -0,0 +1,27 @@
+<script setup lang='ts'>
+import { ref } from 'vue'
+import Check from '@/components/svg/Check.vue'
+import Copy from '@/components/svg/Copy.vue'
+
+const props = defineProps<{
+    content: string
+}>()
+
+const justCopied = ref<boolean>(false)
+
+const copyContent = () => {
+    navigator.clipboard.writeText(props.content)
+    justCopied.value = true
+    setTimeout(() => {
+        justCopied.value = false
+    }, 1000)
+}
+</script>
+
+<template>
+    <Check v-if="justCopied"/>
+    <Copy
+        v-else
+        class="cursor-pointer"
+        @click="copyContent"/>
+</template>
