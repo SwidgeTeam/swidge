2cb39cc09fec5105876e9ec33b7d686b49a96ac2
sandro <devspuig@pm.me>
2022-10-02 00:53:17 +0200

    Update ABI and bytecode, remove duplicated contract

--- contracts/www/tasks/update-queue.js
+++ contracts/www/tasks/update-queue.js
@@ -1,12 +1,6 @@
 const fs = require("fs");
 const path = require("path");
 module.exports = async function (taskArguments, hre, runSuper) {
-  // copy contract for reference
-  fs.copyFileSync(
-    path.join(__dirname, "/../contracts/v0.2/JobsQueue.sol"),
-    path.join(__dirname, "/../../../wrap/src/__tests__/contracts/JobsQueue.sol")
-  );
-
   // update compiled data
   const raw = fs.readFileSync(
     path.join(
