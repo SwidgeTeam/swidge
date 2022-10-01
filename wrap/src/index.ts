b6d5ce7f9a87a83bf668f6fc85e7ff8cbf3d1fa1
sandro <devspuig@pm.me>
2022-10-02 00:51:08 +0200

    Improve readability on wrapper

--- wrap/src/index.ts
+++ wrap/src/index.ts
@@ -4,53 +4,55 @@ import {
   CheckerResult,
   Ethereum_Module,
   Http_Module,
-  GelatoArgs,
   JobResponse,
   Logger_Logger_LogLevel,
   Logger_Module,
   UserArgs
 } from "./wrap";
 
+const getPendingJobsAbi = '{"inputs":[],"name":"getPendingJobs","outputs":[{"components":[{"internalType":"bytes16","name":"id","type":"bytes16"},{"internalType":"address","name":"sender","type":"address"},{"internalType":"address","name":"receiver","type":"address"},{"internalType":"address","name":"inputAsset","type":"address"},{"internalType":"address","name":"dstAsset","type":"address"},{"internalType":"uint256","name":"srcChain","type":"uint256"},{"internalType":"uint256","name":"dstChain","type":"uint256"},{"internalType":"uint256","name":"amountIn","type":"uint256"},{"internalType":"uint256","name":"minAmountOut","type":"uint256"}],"internalType":"struct JobsQueue.ExecuteJob[]","name":"","type":"tuple[]"}],"stateMutability":"view","type":"function"}';
+
 export function checker(args: Args_checker): CheckerResult {
   let userArgs = UserArgs.fromBuffer(args.userArgsBuffer);
-  let gelatoArgs = GelatoArgs.fromBuffer(args.gelatoArgsBuffer);
-
-  let gasPrice = gelatoArgs.gasPrice;
-  let timeStamp = gelatoArgs.timeStamp;
 
   const response = Ethereum_Module.callContractView({
     address: userArgs.queueAddress,
-    method: '{"inputs":[],"name":"getPendingJobs","outputs":[{"components":[{"internalType":"bytes16","name":"id","type":"bytes16"},{"internalType":"address","name":"sender","type":"address"},{"internalType":"address","name":"receiver","type":"address"},{"internalType":"address","name":"inputAsset","type":"address"},{"internalType":"address","name":"dstAsset","type":"address"},{"internalType":"uint256","name":"srcChain","type":"uint256"},{"internalType":"uint256","name":"dstChain","type":"uint256"},{"internalType":"uint256","name":"amountIn","type":"uint256"},{"internalType":"uint256","name":"minAmountOut","type":"uint256"}],"internalType":"struct JobsQueue.ExecuteJob[]","name":"","type":"tuple[]"}],"stateMutability":"view","type":"function"}',
+    method: getPendingJobsAbi,
     args: null,
     connection: args.connection,
   }).unwrap();
 
-  const jobs = <JSON.Arr>JSON.parse(response);
-  const jobsCount = jobs._arr.length;
+  const pendingJobs = <JSON.Arr>JSON.parse(response);
+  const pendingJobsLength = pendingJobs._arr.length;
 
-  if (jobsCount === 0) {
+  if (pendingJobsLength === 0) {
     return nothingToExecute();
   }
 
-  const arr = JSON.Value.Array();
-  const map = jobs._arr.map<JSON.Obj>((val: JSON.Value): JSON.Obj => {
-    const obj = JSON.Value.Object();
-    obj.set('jobId', (val as JSON.Obj).get('id'))
-    const jobResponse = getCallData(val as JSON.Obj);
-    obj.set('handler', jobResponse.handler);
-    obj.set('data', jobResponse.data);
-    return obj;
+  const jobs = pendingJobs._arr.map<JSON.Obj>((item: JSON.Value): JSON.Obj => {
+    const pendingJob = item as JSON.Obj;
+    const jobResponse = getCallData(pendingJob);
+    const job = JSON.Value.Object();
+    job.set('jobId', pendingJob.get('id'));
+    job.set('providerInfo', pendingJob.get('providerInfo'));
+    job.set('handler', jobResponse.handler);
+    job.set('data', jobResponse.data);
+    return job;
   });
 
-  const argsSig: string[] = [];
-  for (let i = 0; i < map.length; i++) {
-    arr.push(map[i]);
-    argsSig.push('tuple(bytes16 jobId,address handler,bytes data)')
+  const jobsArr = JSON.Value.Array();
+  const argsSignature: string[] = [];
+  // doing this extra loop because can't push element
+  // into an Array inside a map iterator : Not implemented error (¿?)
+  // but we need an Array to be able to stringify the elements
+  for (let i = 0; i < jobs.length; i++) {
+    jobsArr.push(jobs[i]);
+    argsSignature.push('tuple(bytes16 jobId,address handler,bytes data)')
   }
 
   const encoded = Ethereum_Module.encodeFunction({
-    method: 'function executeJobs (tuple(' + argsSig.join(',') + '))',
-    args: [arr.stringify()],
+    method: `function executeJobs (tuple(${argsSignature.join(',')}))`,
+    args: [jobsArr.stringify()],
   });
 
   let execData = encoded.unwrap();
