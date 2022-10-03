384201107601859cafcdcdc727d00511ada9aade
Hublax <hubert.baciocha@gmail.com>
2022-10-03 15:01:12 +0200

    Add Template

--- front/www/src/components/Modals/ModalTransactionStatus.vue
+++ front/www/src/components/Modals/ModalTransactionStatus.vue
@@ -1,13 +1,20 @@
 <script setup lang='ts'>
-import StatusStep from '../StatusStep.vue'
 import { useRoutesStore } from '@/store/routes'
 import Route from '@/domain/paths/path'
-import Modal from '@/components/Modals/Modal.vue'
+import ModalFull from '@/components/Modals/ModalFull.vue'
+import SwidgeLogo from '../svg/SwidgeLogo.vue';
+import TokenLogo from '@/components/Icons/TokenLogo.vue'
+import ChainLogo from '@/components/Icons/ChainLogo.vue'
+import { IToken } from '@/domain/metadata/Metadata'
+import { useMetadataStore } from '@/store/metadata'
 
 const routesStore = useRoutesStore()
+const metadataStore = useMetadataStore()
 
-defineProps<{
+
+const props = defineProps<{
     show: boolean
+    token: IToken
 }>()
 
 const emits = defineEmits<{
@@ -18,21 +25,53 @@ const getRoute = (): Route => {
     return routesStore.getSelectedRoute
 }
 
+const getChainLogo = () => {
+    const chain = metadataStore.getChain(props.token.chainId)
+    return chain ? chain.logo : ''
+}
+
 </script>
 
 <template>
-    <Modal
+    <ModalFull
         :is-open="show"
         @close="emits('close-modal')">
 
-        <StatusStep
-            v-for="(step, index) in getRoute().steps"
-            :key="index"
-            :step="step"
-        />
+        <div class="grid grid-rows-5 flex">
+            <div class="flex justify-start">                        
+                <SwidgeLogo
+                    class="xs:w-24 sm:w-32 w-32 top-2 left-2  cursor-pointer"
+                 />
+            </div>
+            <div class="flex justify-center">
+                Timer
+            </div>
+            <div class="flex justify-center">
+                <div>
+                    <div class="relative w-6">
+                        From Token
+                    </div>
+                </div>
+                <div>
+                    arrow
+                </div>
+                <div>
+                    To token
+                </div>
+            </div>
+            <div class="grid grid-rows-2 flex justify-center">
+                <div>Start Chain Hash</div>
+                <div>Dest. Chain Hash</div>
+            </div>
+            <div class="grid grid-rows-2 flex justify-center">
+                <div>Twitter</div>
+                <div>Discord</div>
+            </div>
+            
+        </div>
 
         <div v-if="getRoute().completed" class="flex flex-col items-center">
             <div class="text-3xl font-bold">Swidge successful!</div>
         </div>
-    </Modal>
+    </ModalFull>
 </template>
