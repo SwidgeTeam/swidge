44613757544425c7b4dfe65bb7f0874090f5484d
sandro <devspuig@pm.me>
2022-10-06 10:45:05 +0200

    Add extra details on tx status endpoint

--- api/www/src/transactions/application/query/check-tx-status-handler.ts
+++ api/www/src/transactions/application/query/check-tx-status-handler.ts
@@ -5,6 +5,7 @@ import { Logger } from '../../../shared/domain/logger';
 import { ConfigService } from '../../../config/config.service';
 import CheckTxStatusQuery from './check-tx-status-query';
 import { TransactionsRepository } from '../../domain/TransactionsRepository';
+import { Transaction } from '../../domain/Transaction';
 
 @QueryHandler(CheckTxStatusQuery)
 export class CheckTxStatusHandler implements IQueryHandler<CheckTxStatusQuery> {
@@ -14,9 +15,8 @@ export class CheckTxStatusHandler implements IQueryHandler<CheckTxStatusQuery> {
     @Inject(Class.Logger) private readonly logger: Logger,
   ) {}
 
-  async execute(query: CheckTxStatusQuery): Promise<string> {
+  async execute(query: CheckTxStatusQuery): Promise<Transaction | null> {
     this.logger.log(`Checking status of tx ${query.txId}`);
-    const tx = await this.repository.find(query.txId);
-    return tx ? tx.status : '';
+    return await this.repository.find(query.txId);
   }
 }
