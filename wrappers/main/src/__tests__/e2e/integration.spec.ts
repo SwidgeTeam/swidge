e49ebbf02d3eccacc982576fd657ab5c6acf53cf
sandro <devspuig@pm.me>
2022-10-29 19:51:29 +0200

    Add logic on main wrapper to group metadata

--- wrappers/main/src/__tests__/e2e/integration.spec.ts
+++ wrappers/main/src/__tests__/e2e/integration.spec.ts
@@ -5,7 +5,17 @@ jest.setTimeout(60000);
 
 describe("Template Wrapper End to End Tests", () => {
 
-  const client: PolywrapClient = new PolywrapClient();
+  const client: PolywrapClient = new PolywrapClient({
+    interfaces: [
+      {
+        interface: "wrap://ipfs/QmbaTvgNrDafcChhFZw83aSKMq57iyWwdvWzuMhWKmgj55",
+        implementations: [
+          "wrap://ipfs/QmNfgGbY8s1TRr8mRahE6RxDzsBkj1o7a6VJs2ne6mGFgB",
+          "wrap://ipfs/QmVZptPRpj4KyER6Ry1wzm4HNKBxRPS4Gs6Xhoy2tmg4kp"
+        ]
+      }
+    ]
+  });
   let wrapperUri: string;
 
   beforeAll(() => {
@@ -15,13 +25,16 @@ describe("Template Wrapper End to End Tests", () => {
   })
 
   it("calls sampleMethod", async () => {
-    const result = await client.invoke({
+    const result = await client.invoke<{
+      ok: boolean,
+      value: any
+    }>({
       uri: wrapperUri,
       method: "getMetadata",
       args: {}
     });
 
-    console.log(result);
+    console.log(result.value);
 
     expect(result.ok).toBeTruthy();
     if (!result.ok) return;
--- wrappers/main/src/metadata.rs
+++ wrappers/main/src/metadata.rs
@@ -1,17 +1,78 @@
 use std::collections::BTreeMap;
-use crate::imported::rango_module;
-use crate::imported::lifi_module;
 use crate::wrap::*;
+use crate::imported::provider_module::ArgsGetMetadata as ProviderArgsGetMetadata;
 
 pub fn get_metadata(args: ArgsGetMetadata) -> Metadata {
-    let rango_metadata = RangoModule::get_metadata(&rango_module::serialization::ArgsGetMetadata {});
-    let lifi_metadata = LifiModule::get_metadata(&lifi_module::serialization::ArgsGetMetadata {});
+    let implementations = Provider::get_implementations();
 
-    let ran = rango_metadata.unwrap().chains;
-    let lifi = lifi_metadata.unwrap().chains;
+    if implementations.len() == 0 {
+        panic!("No provider implementations");
+    }
+
+    let mut all_chains: Vec<Chain> = Vec::new();
+    let mut all_tokens: BTreeMap<String, Vec<Token>> = BTreeMap::new();
+
+    for implementation in implementations.iter() {
+        let provider = ProviderModule::new(implementation);
+        let metadata = provider
+            .get_metadata(&ProviderArgsGetMetadata {})
+            .expect("Provider should return valid metadata");
+
+        metadata.chains
+            .iter()
+            .for_each(|chain| {
+                let exists = all_chains
+                    .iter()
+                    .find(|c| c.chain_id == chain.chain_id)
+                    .is_some();
+
+                if !exists {
+                    all_chains.push(Chain {
+                        chain_type: chain.chain_type.to_string(),
+                        chain_id: chain.chain_id.to_string(),
+                        name: chain.name.to_string(),
+                        logo: chain.logo.to_string(),
+                    });
+                }
+            });
+
+        metadata.tokens
+            .iter()
+            .for_each(|(chain_id, tokens)| {
+                if !all_tokens.contains_key(chain_id) {
+                    all_tokens.insert(chain_id.clone(), Vec::new());
+                }
+
+                let all_tokens_chain = all_tokens
+                    .get_mut(chain_id)
+                    .expect("Should exists chain, it was checked just above");
+
+                tokens
+                    .iter()
+                    .for_each(|token| {
+                        let exists = all_tokens_chain
+                            .iter()
+                            .find(|t| t.address.eq(token))
+                            .is_some();
+
+                        if !exists {
+                            all_tokens_chain.push(Token {
+                                chain_id: "".to_string(),
+                                address: token.to_string(),
+                                name: "".to_string(),
+                                symbol: "".to_string(),
+                                decimals: 0,
+                                logo: None,
+                                price: None,
+                            });
+                        }
+                    })
+            })
+    }
 
     return Metadata {
-        chains: vec![],
-        tokens: BTreeMap::new(),
+        chains: all_chains,
+        tokens: all_tokens,
     };
 }
+
